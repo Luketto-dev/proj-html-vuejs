57a1d3ee6caf22ca1f013b47d11dbcb2b31ae062
Luketto-dev <luca.francesconi1995@gmail.com>
2022-07-01 12:56:01 +0200

    inizializzazione progetto

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="my-navbar">
+                <div class="my-navbar-logo">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            headerLinks:[
+                "Home",
+                "who we are",
+                "What We Do",
+                "where We Work",
+                "Careers",
+                "News",
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
